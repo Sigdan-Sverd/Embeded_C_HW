07e56051abe9908954eccdbf76bc9eabab520329
Sigdan Sverð <graograman18@gmail.com>
2026-09-16 22:34:12 +0300

    HW1. If you are not the lecturer of this course, STAY AWAY FROM THE NONSENSE I CREATEDgit add .!

---
+++ HW1/Task3.c
@@ -0,0 +1,27 @@
+/*
+ * Task3.c
+ * 
+ * 
+ * This program has no productive purpose and 
+ * has no recommendation  for use WHAT SO EVER 
+ * 
+ * If you ever come across this...  
+ * May God bless your soul
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf ("     *\n");
+	printf ("    ***\n");
+	printf ("   *****\n");
+	printf ("  *******\n");
+	printf ("   HHOHH\n");
+	printf ("   ZZZZZ\n");
+	return 0;
+}
+
