d2a3b4f7774919f1f90edfbd76230b596019aaff
Sigdan Sverð <graograman18@gmail.com>
2026-09-16 22:20:20 +0300

    If you are not the lecturer of this course, STAY AWAY FROM THIS NONSENSEgit add .!

---
+++ HW1/Sigdan-SverdHW11.c
@@ -0,0 +1,28 @@
+/*
+ * Sigdan-SverdHW1.1.c
+ * 
+ * 
+ * This program has no productive purpose and 
+ * has no recommendation  for use WHAT SO EVER 
+ * 
+ * If you ever come across this...  
+ * May God bless your soul
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf ("	 Let's\n");
+	printf ("           go\n");
+	printf ("             to walk\n");
+	//printf ("Has he lost his mind?\n");
+	//printf ("Can he see, or is he blind?\n");
+	//printf ("Can he walk at all?\n");
+	//printf ("Or if he moves, will he fall?\n");
+	return 0;
+}
+
